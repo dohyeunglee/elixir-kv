8d1da4bfd77d3bef0af1e4708266452f7953050b
dohyeunglee <dohyeunglee@snu.ac.kr>
2018-05-01 15:48:03 +0900

    first commit;

---
+++ apps/kv/test/kv/bucket_test.exs
@@ -0,0 +1,19 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  setup do
+    bucket = start_supervised!(KV.Bucket)
+    %{bucket: bucket}
+  end
+
+  test "are temporary workers" do
+    assert Supervisor.child_spec(KV.Bucket, []).restart == :temporary
+  end
+
+  test "stores values by keys", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+end
